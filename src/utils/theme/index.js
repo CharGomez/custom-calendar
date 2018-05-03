dd73b3a3fd4c7d33e0883ab28c087bf72bae8bbb
Carlos Gomez <carlos.gomez@dnamicworld.com>
2018-05-03 13:15:41 -0600

    fix theme - config

--- src/utils/theme/index.js
+++ src/utils/theme/index.js
@@ -1,8 +1,10 @@
-const colors = {
-  white: '#FFFFFF',
-  black: '#000000',
-  transparent: 'rgba(0, 0, 0, 0)',
-  shadow: opacity => `rgba(255,255,255,0.${opacity})`,
+const theme = {
+  colors: {
+    white: '#FFFFFF',
+    black: '#000000',
+    transparent: 'rgba(0, 0, 0, 0)',
+    shadow: opacity => `rgba(255,255,255,0.${opacity})`,
+  },
 }
 
-export { colors }
+export { theme }
